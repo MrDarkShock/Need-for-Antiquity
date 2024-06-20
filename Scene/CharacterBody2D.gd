3a3aed73593bef307a36c02b06025abcc2333d60
Vallauth <valentin.la@ludus-academie.fr>
2024-06-20 12:04:43 +0200

    vie

--- Scene/CharacterBody2D.gd
+++ Scene/CharacterBody2D.gd
@@ -9,10 +9,18 @@ func _ready():
 	Global.points = 0	
 	$score.start ()
 	$CharacterBody2D/AnimatedSprite2D.play()
+	Global.vie = 3
 	# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	generate_obs()
 	$CharacterBody2D/Label2.text = str(Global.points)
+	if Global.vie == 2 :
+		$CharacterBody2D/Hearts2.hide()
+	else :
+		if Global.vie == 1 :
+			$CharacterBody2D/Hearts3.hide()
+		if Global.vie == 0 :
+			$CharacterBody2D/Hearts1.hide()
 
 
 
